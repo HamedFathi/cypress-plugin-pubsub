4f8c64dd5fc1978dd060098e715aa64f2fcfbc54
hamedfathi <hamedfathi@outlook.com>
2021-08-12 18:50:28 +0430

    feat: declaration file added

---
+++ dist/index.d.ts
@@ -0,0 +1,21 @@
+/// <reference types="cypress" />
+/// <reference types="pubsub-js" />
+
+declare module Cypress {
+    interface Chainable<Subject = any> {
+        subscribe(message: string | Symbol, func: Function): Chainable<Subject>;
+        subscribeOnce(message: string | Symbol, func: Function): Chainable<Subject>;
+        countSubscriptions(token: any): Chainable<Subject>;
+        clearAllSubscriptions(token?: any): Chainable<Subject>;
+        getSubscriptions(token: any): Chainable<Subject>;
+        publish(message: string | Symbol, data?: any): Chainable<Subject>;
+        publishSync(message: string | Symbol, data?: any): Chainable<Subject>;
+        unsubscribe(tokenOrFunction: any): Chainable<Subject>;
+    }
+}
+
+declare module global {
+    interface Window {
+        PubSub: PubSubJS.Base;
+    }
+}
